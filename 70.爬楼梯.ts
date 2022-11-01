0dec125865169ee8be385608460a5880e66add13
sunnywind <sunny-wind@foxmail.com>
2022-11-01 15:54:27 +0800

    70

---
+++ 70.爬楼梯.ts
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=typescript
+ *
+ * [70] 爬楼梯
+ */
+
+// @lc code=start
+function climbStairs(n: number): number {
+  let dp = [1, 1];
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
+};
+// @lc code=end
+
