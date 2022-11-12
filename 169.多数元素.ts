fd04a9ddaf51f4d47d4d1d3e6804342afb43367c
sunnywind <sunny-wind@foxmail.com>
2022-11-12 15:20:59 +0800

    169

---
+++ 169.多数元素.ts
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=169 lang=typescript
+ *
+ * [169] 多数元素
+ */
+
+// @lc code=start
+function majorityElement(nums: number[]): number {
+  let count = 0;
+  let candidate = null;
+  for (const num of nums) {
+    if (count === 0) {
+      candidate = num;
+    }
+    count += num === candidate ? 1 : -1;
+  }
+  return candidate;
+};
+// @lc code=end
+
