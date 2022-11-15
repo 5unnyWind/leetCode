f9106ed18be5208b7b1eebdc34597b13d04340fc
sunnywind <sunny-wind@foxmail.com>
2022-11-15 18:51:54 +0800

    234

---
+++ 234.回文链表.ts
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=234 lang=typescript
+ *
+ * [234] 回文链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function isPalindrome(head: ListNode | null): boolean {
+    let arr = [];
+    while (head) {
+        arr.push(head.val);
+        head = head.next;
+    }
+    let left = 0;
+    let right = arr.length - 1;
+    while (left < right) {
+        if (arr[left] !== arr[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+};
+// @lc code=end
+
