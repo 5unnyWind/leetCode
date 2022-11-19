bc75a47a823ab3d03abb02fde72e870051a6abfe
sunnywind <sunny-wind@foxmail.com>
2022-11-19 15:16:47 +0800

    461

---
+++ 461.汉明距离.ts
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=461 lang=typescript
+ *
+ * [461] 汉明距离
+ */
+
+// @lc code=start
+function hammingDistance(x: number, y: number): number {
+  let z = x ^ y;
+  let ret = 0;
+  while (z) {
+    z &= z - 1;
+    ret++;
+  }
+  return ret;
+};
+// @lc code=end
+
