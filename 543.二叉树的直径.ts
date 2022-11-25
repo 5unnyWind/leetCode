9935545363b4da6d70c386e0753d29f8586b5a95
sunnywind <sunny-wind@foxmail.com>
2022-11-25 21:47:30 +0800

    543

---
+++ 543.二叉树的直径.ts
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=543 lang=typescript
+ *
+ * [543] 二叉树的直径
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function diameterOfBinaryTree(root: TreeNode | null): number {
+  let max = 0;
+  const dfs = (root: TreeNode | null): number => {
+    if (!root) return 0;
+    const left = dfs(root.left);
+    const right = dfs(root.right);
+    max = Math.max(max, left + right);
+    return Math.max(left, right) + 1;
+  };
+  dfs(root);
+  return max;
+};
+// @lc code=end
+
