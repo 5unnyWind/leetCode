2fd89195618300a112f997c2368b73a004c2b4ba
sunnywind <sunny-wind@foxmail.com>
2022-12-02 13:41:18 +0800

    15

---
+++ 15.三数之和.ts
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=15 lang=typescript
+ *
+ * [15] 三数之和
+ */
+
+// @lc code=start
+function threeSum(nums: number[]): number[][] {
+  let res: number[][] = [];
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (nums[i] > 0) break;
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    let l = i + 1;
+    let r = nums.length - 1;
+    while (l < r) {
+      const sum = nums[i] + nums[l] + nums[r];
+      if (sum === 0) {
+        res.push([nums[i], nums[l], nums[r]]);
+        while (l < r && nums[l] === nums[l + 1]) l++;
+        while (l < r && nums[r] === nums[r - 1]) r--;
+        l++;
+        r--;
+      } else if (sum < 0) {
+        l++;
+      } else if (sum > 0) {
+        r--;
+      }
+    }
+  }
+  return res;
+};
+// @lc code=end
+
